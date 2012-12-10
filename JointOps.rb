79e8a3a59d6131231422b5dfd5b5be52522e1003
hookahfire <rebelliousant@gmail.com>
2012-12-10 13:28:05 -0700

    Redo

--- JointOps.rb
+++ JointOps.rb
@@ -12,11 +12,7 @@ require 'socket'
 # a client-server relationship, I figure it would
 # be useful to have the local IP made readily available
 def local_ip
-<<<<<<< HEAD
   orig = Socket.do_not_reverse_lookup
-=======
-  orig = Socket.do_not_reverse_lookup  
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
   Socket.do_not_reverse_lookup =true # turn off reverse DNS resolution temporarily
   begin #make sure lack of internet connection doesn't kill the script
     UDPSocket.open do |s|
@@ -33,7 +29,6 @@ end
 # program should have a window both users can edit(main project)
 # program should have chat window
 # program should text box
-<<<<<<< HEAD
 
 # might want to append current file name to window title
 #long variable name can be changed
@@ -53,12 +48,6 @@ end
 Gtk.init
 window = Gtk::Window.new( "Joint Ops #{currentsourcecode}" )
 window.set_size_request( 600, 400 )
-=======
-# might want to append current file name to window title
-Gtk.init
-window = Gtk::Window.new( 'Joint Ops' )
-window.set_default_size( 600, 400 )
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
 window.signal_connect( 'destroy') {
   Gtk.main_quit
 }
@@ -68,16 +57,10 @@ window.signal_connect( 'destroy') {
 #
 file_menu = Gtk::Menu.new
   file_open = Gtk::MenuItem.new( 'Open' )
-<<<<<<< HEAD
   file_save = Gtk::MenuItem.new( 'Save' )
   file_exit = Gtk::MenuItem.new( 'Exit' )
   file_exit.signal_connect( 'activate' ) { Gtk.main_quit }
   file_menu.add( file_open ).add( file_save ).add( file_exit )
-=======
-  file_exit = Gtk::MenuItem.new( 'Exit' )
-  file_exit.signal_connect( 'activate' ) { Gtk.main_quit }
-  file_menu.add( file_open ).add( file_exit )
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
   
 menu_file = Gtk::MenuItem.new( '_File' )
   menu_file.set_submenu( file_menu )
@@ -103,11 +86,7 @@ scrolltb.add( textbox )
 #
 # Chat Window
 # editable should be false
-<<<<<<< HEAD
 # should have it's text buffer
-=======
-# should have it's text buffer 
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
 # linked to the message box text buffer
 # method should be in place to clear the text and send the value
 # of it to both users' screens
@@ -119,45 +98,34 @@ raise 'Must not run as root' unless Process.uid != 0
 chat = Gtk::TextView.new
 chat.wrap_mode = Gtk::TextTag::WRAP_WORD
 chat.editable = false
-<<<<<<< HEAD
 chat.cursor_visible = false
-=======
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
 scrollch = Gtk::ScrolledWindow.new
 scrollch.add( chat )
 #
 # Text Box for Chat Window
 #
 message = Gtk::Entry.new
-<<<<<<< HEAD
 # get message to accept keypress
 window.add_events( Gdk::Event::KEY_PRESS )
 message.signal_connect( 'key-press-event' ) { |w,e|
-  if ( message.text == ( '' || message.text.match(/\ */) ) ) then
-    
-  else
-    if ( e.keyval == 65293 ) then
-      insert_text( message, chat )
-    elsif ( e.keyval == 65421 ) then
+  if ( message.text =~ /[a-zA-Z0-9]+/ ) then
+    if ( e.keyval == 65293 ) || ( e.keyval == 65421 ) then
       insert_text( message, chat )
     end
+  else
+  
   end
 }
 #
 # Text Box send button
 send = Gtk::Button.new( 'Send' )
 send.signal_connect( 'clicked' ) {
-  if ( message.text == ( '' || message.text.match(/\ */) ) ) then
-    
-  else
+  if ( message.text =~ /[a-zA-Z0-9]+/ ) then
     insert_text( message, chat )
+  else
+    
   end
 }
-=======
-#
-# Text Box send button
-send = Gtk::Button.new( 'Send' )
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
 # some function to add the text in the message box
 # into the textbox
 userlbl = Gtk::Label.new ( $username )
@@ -170,33 +138,21 @@ hbox1 = Gtk::HBox.new( false, 0 )
 # should control the size of the code and chat windows
 # should make the code window 3/4 of give space
 # should make the chat window 1/4 of give space
-<<<<<<< HEAD
-# packing
-table = Gtk::Table.new( 4, 4, true )
-table.attach_defaults( scrolltb, 0, 3, 0, 4 )
-table.attach_defaults( scrollch, 3, 4, 0, 4 )
-
-=======
 # size0 = Gtk::Alignment.new( 0, 0, 0.75, 1 )
 
 
 # packing
 # size0.add( hbox0 )
+table = Gtk::Table.new( 4, 4, true )
+table.attach_defaults( scrolltb, 0, 3, 0, 4 )
+table.attach_defaults( scrollch, 3, 4, 0, 4 )
 
-
-hbox0.pack_start( scrolltb, true, true, 0 )
-hbox0.pack_start( scrollch, true, true, 0 )
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
 hbox1.pack_start( userlbl, false, false, 0 )
 hbox1.pack_start( message, true, true, 0 )
 hbox1.pack_start( send, false, false, 0 )
 
 vbox0.pack_start( menu_bar, false, false, 0 )
-<<<<<<< HEAD
 vbox0.pack_start( table, true, true, 0 )
-=======
-vbox0.pack_start( hbox0, true, true, 0 )
->>>>>>> 54544780c5957db91d8500a739caa53481282c37
 
 vbox0.pack_start( hbox1, false, false, 0 )
 
@@ -205,8 +161,4 @@ window.add( vbox0 )
 # show it all
 window.show_all
 
-<<<<<<< HEAD
 Gtk.main
-=======
-Gtk.main
->>>>>>> 54544780c5957db91d8500a739caa53481282c37

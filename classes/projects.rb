84f58951be84910653d7274326d328ba5809c2be
jointcoders <cmontgomeryburns@gmail.com>
2013-01-16 17:26:20 -0800

    Had a new idea, clean up the workspace and separate things out. Turn projects to be opened into a new class of type project, work in progress.

---
+++ classes/projects.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+class Project
+  def initialize (username, project_name, file_name)
+    @username = username
+    @project_name = project_name
+    @file_name = file_name
+  end
+end
+

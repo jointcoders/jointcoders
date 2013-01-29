a4c0858e5994c28f4770ea03e84395f7ea0e6686
jointcoders <cmontgomeryburns@gmail.com>
2013-01-29 00:25:36 -0800

    Added some more tests in converting to QT but also added the rubygemcheck piece to neatly check for dependencies.

---
+++ rubygemcheck.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+#
+# This is a little thing that will run and you
+# can just add whatever gems are needed to the
+# required gems at the top. If any fail to load
+# it will print a failure, add them to an array
+# and print the array on exit listing all failures
+ 
+required_gems = ['qtbindings']
+missing_gems = Array.new
+required_gems.each do |testgem|
+  begin
+    gem testgem
+    # with requirements
+    #gem "qtbindings", ">=4.0"
+    puts "Gem #{testgem} is installed.....\033[32mOK\033[0m"
+  rescue Gem::LoadError, NameError => reason
+    # not instailled
+    puts "Gem #{testgem} is installed.....\033[31mFAIL\033[0m"
+    #missing_gems.push "#{testgem}\n"
+    missing_gems.push testgem
+  end
+end
+
+if missing_gems.any? then
+  puts ''
+  puts 'The following gems failed to load:'
+  missing_gems.each do |gem|
+    puts gem
+  end
+  exit
+end
